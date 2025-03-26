aa8f7dcab69335251cdef0a8a439102aad4de8eb
Twixmaximus <134007646+marctina1992@users.noreply.github.com>
2025-03-26 12:22:27 +0000

    modified:   assets/images/opensis_logo.png 	modified:   assets/opensis-logo.png 	modified:   assets/opensis_logo.png 	modified:   assets/os4ed_logo.png 	modified:   assets/osis_logo.png 	modified:   assets/small-logo.png

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = 'root'; 
+$DatabaseName = 'database1'; 
+$DatabasePort = '3306'; 
+?>
